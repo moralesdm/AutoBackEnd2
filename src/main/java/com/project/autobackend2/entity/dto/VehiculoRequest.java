81d51a59f8757280c2884305a8f6b0322d18b645
moralesdm <morales.david.gr.22@gmail.com>
2025-06-20 21:38:42 -0500

    feat(dto): add VehiculoRequest DTO for vehicle creation

---
+++ src/main/java/com/project/autobackend2/entity/dto/VehiculoRequest.java
@@ -0,0 +1,14 @@
+package com.project.autobackend2.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class VehiculoRequest {
+    private String marca;
+    private String modelo;
+    private String tipo;
+    private String anio;
+    private String color;
+    private Long categoriaId;
+    private Long sucursalId;
+}

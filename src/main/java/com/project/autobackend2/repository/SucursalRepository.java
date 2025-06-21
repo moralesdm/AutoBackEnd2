fc8910a518b704b4cb67302a4268fc274828e4d5
moralesdm <morales.david.gr.22@gmail.com>
2025-06-20 21:10:42 -0500

    feat(repository): add SucursalRepository to support vehicle location data

---
+++ src/main/java/com/project/autobackend2/repository/SucursalRepository.java
@@ -0,0 +1,8 @@
+package com.project.autobackend2.repository;
+
+import com.project.autobackend2.entity.dto.Sucursal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SucursalRepository extends JpaRepository<Sucursal,Long> {
+
+}

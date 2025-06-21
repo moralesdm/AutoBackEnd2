803d8a707f46caec0b48fa4f72ee8a6478f0712e
moralesdm <morales.david.gr.22@gmail.com>
2025-06-20 20:20:51 -0500

    first commit

---
+++ src/main/java/com/project/autobackend2/AutoBackEnd2Application.java
@@ -0,0 +1,13 @@
+package com.project.autobackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoBackEnd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoBackEnd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/autobackend2/AutoBackEnd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.autobackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBackEnd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

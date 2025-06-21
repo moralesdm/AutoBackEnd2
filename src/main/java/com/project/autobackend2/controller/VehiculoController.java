40e3b64c7f0f329e0249ad3105daa6ac91ce3aee
moralesdm <morales.david.gr.22@gmail.com>
2025-06-20 21:40:50 -0500

    feat(controller): add POST /api/vehiculos endpoint to create new vehicle

---
+++ src/main/java/com/project/autobackend2/controller/VehiculoController.java
@@ -0,0 +1,26 @@
+package com.project.autobackend2.controller;
+
+import com.project.autobackend2.entity.dto.VehiculoRequest;
+import com.project.autobackend2.entity.dto.VehiculoResponse;
+import com.project.autobackend2.service.VehiculoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vehiculos")
+@RequiredArgsConstructor
+public class VehiculoController {
+
+    private final VehiculoService vehiculoService;
+
+    @PostMapping
+    public ResponseEntity<VehiculoResponse> crearVehiculo(@RequestBody VehiculoRequest request) {
+        VehiculoResponse vehiculo = vehiculoService.crearVehiculo(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(vehiculo);
+    }
+}

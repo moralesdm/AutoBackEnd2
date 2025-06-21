97eb639bd2b5d8a0772d5729b5a1f7fa91ff59c4
moralesdm <morales.david.gr.22@gmail.com>
2025-06-20 21:10:02 -0500

    feat(repository): add VehiculoRepository for database access

---
+++ src/main/java/com/project/autobackend2/repository/VehiculoRepository.java
@@ -0,0 +1,8 @@
+package com.project.autobackend2.repository;
+
+import com.project.autobackend2.entity.dto.Vehiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehiculoRepository extends JpaRepository<Vehiculo,Long> {
+
+}

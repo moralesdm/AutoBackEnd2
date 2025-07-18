7fe39c36e748e4377324bd600c6fced657f769ed
moralesdm <morales.david.gr.22@gmail.com>
2025-07-18 15:26:36 -0500

    fix(file): update files

--- src/main/java/com/project/autobackend2/controller/VehiculoController.java
+++ src/main/java/com/project/autobackend2/controller/VehiculoController.java
@@ -18,13 +18,13 @@ public class VehiculoController {
 
     private final VehiculoService vehiculoService;
 
-    @PostMapping
+    @PostMapping("/")
     public ResponseEntity<VehiculoResponse> crearVehiculo(@RequestBody VehiculoRequest request) {
         VehiculoResponse vehiculo = vehiculoService.crearVehiculo(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(vehiculo);
     }
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<List<VehiculoResponse>> listarVehiculos() {
         return ResponseEntity.ok(vehiculoService.listarVehiculos());
     }
